c71ff2267cc6eab878dd5aed2c2f479b7e6ece8a
kechankrisna <ke.chankrisna168@gmail.com>
2021-04-01 15:21:46 +0700

    export

--- lib/pos_printer_manager.dart
+++ lib/pos_printer_manager.dart
@@ -9,7 +9,11 @@ export 'models/network_printer.dart';
 export 'models/bluetooth_printer.dart';
 export 'services/bluetooth_printer_manager.dart';
 export 'services/network_printer_manager.dart';
+export 'enums/bluetooth_printer_type.dart';
+export 'enums/connection_response.dart';
+export 'enums/connection_type.dart';
 export 'package:esc_pos_utils_plus/esc_pos_utils.dart';
+
 
 class PosPrinterManager {
   static const MethodChannel _channel =
